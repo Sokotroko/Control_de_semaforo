2dfa123fa59bcb0ec8dc4a760b6fefdb171a5b49
Sokotroko <144294565+Sokotroko@users.noreply.github.com>
2024-07-25 02:00:59 -0500

    Delete frontend - copia directory

--- frontend - copia/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:8000/api/';  // URL de la API Django
-
-export const getOverview = async () => {
-    const response = await axios.get(API_URL);
-    return response.data;
-};
